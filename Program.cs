f1a2a3e030aa9d7c8b05c621285aee6dc8f84964
Triz <triz.marciano@gmail.com>
2024-10-24 19:41:26 -0300

    Metodo testado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia (numero: "1234", modelo: "model1", imei: "1111", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone: ");
+Smartphone iphone = new Iphone (numero: "5678", modelo: "model2", imei: "2222", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snapchat");
