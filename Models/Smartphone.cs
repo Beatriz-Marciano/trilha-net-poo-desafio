358574c280559de38b42957ea0409c55fa26ee81
Triz <triz.marciano@gmail.com>
2024-10-24 19:40:10 -0300

    Implementado as propriedades faltantes de acordo com o diagrama

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,20 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
+        
+        // Implementado as propriedades faltantes de acordo com o diagrama!!!
+        
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
+            // Implementado os parâmetros do construtor para as propriedades!!!
         }
 
         public void Ligar()
